f3ab02bc00aa89835f98f24bc505fd071429848c
hruthikn9-crypto <hruthikn9@gmail.com>
2025-10-28 10:56:44 +0530

    Create file3.c

---
+++ file3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int num; // Declare an integer variable
+    printf("Enter an integer: ");
+    scanf("%d", &num); // Read an integer from the user
+    printf("You entered: %d\n", num); // Print the entered integer
+    return 0;
+}
